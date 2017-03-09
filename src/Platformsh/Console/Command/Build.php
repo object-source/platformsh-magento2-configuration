475a1211943502ce27c89a38c069eda20a18d097
Paul Partington <p.partington@ctidigital.com>
2017-03-09 12:13:24 +0000

    Add option to skip patches.

--- src/Platformsh/Console/Command/Build.php
+++ src/Platformsh/Console/Command/Build.php
@@ -21,6 +21,7 @@ class Build extends Command
      */
     const BUILD_OPT_SKIP_DI_COMPILATION = 'skip_di_compilation';
     const BUILD_OPT_SKIP_DI_CLEARING = 'skip_di_clearing';
+    const BUILD_OPT_SKIP_PATCHES = 'skip-patches';
 
     /**
      * @var Environment
@@ -56,7 +57,9 @@ class Build extends Command
     private function build()
     {
         $this->env->log("Start build.");
-        $this->applyMccPatches();
+        if (!$this->getBuildOption(self::BUILD_OPT_SKIP_PATCHES)) {
+            $this->applyMccPatches();
+        }
         $this->applyCommittedPatches();
         $this->compileDI();
         $this->clearInitDir();
