7e830e93af6fa102c98bd998ea484a617d4f96f6
Paul Partington <p.partington@ctidigital.com>
2017-10-23 13:43:32 +0100

    Update build script to try and compile the theme.

--- src/Platformsh/Console/Command/Build.php
+++ src/Platformsh/Console/Command/Build.php
@@ -21,6 +21,9 @@ class Build extends Command
      */
     const BUILD_OPT_SKIP_DI_COMPILATION = 'skip_di_compilation';
     const BUILD_OPT_SKIP_DI_CLEARING = 'skip_di_clearing';
+    const BUILD_OPT_SCD_EXCLUDE_THEMES = 'exclude_themes';
+    const BUILD_OPT_SCD_THREADS = 'scd_threads';
+    const BUILD_OPT_SKIP_SCD = 'skip_scd';
 
     /**
      * @var Environment
@@ -65,9 +68,9 @@ class Build extends Command
         $this->setEnvData();
         $this->applyMccPatches();
         $this->applyCommittedPatches();
-        //$this->importConfiguration();
         $this->compileDI();
-        $this->generateFreshStaticContent();
+        $this->composerDumpAutoload();
+        $this->deployStaticContent();
         $this->clearInitDir();
         $this->env->execute('rm -rf app/etc/env.php');
 
@@ -75,7 +78,20 @@ class Build extends Command
          * Writable directories will be erased when the writable filesystem is mounted to them. This
          * step backs them up to ./init/
          */
+        $this->env->log("Moving static content to init directory");
+        $this->env->execute('mkdir -p ./init/pub/');
+        if (file_exists('./init/pub/static')) {
+            $this->env->log("Remove ./init/pub/static");
+            unlink('./init/pub/static');
+        }
+        $this->env->execute('cp -R ./pub/static/ ./init/pub/static');
+        copy(
+            Environment::MAGENTO_ROOT . '/.static_content_deploy',
+            Environment::MAGENTO_ROOT . 'init/' . '/.static_content_deploy'
+        );
+
         $this->env->log("Copying writable directories to temp directory.");
+
         foreach ($this->env->writableDirs as $dir) {
             $this->env->execute(sprintf('mkdir -p init/%s', $dir));
             $this->env->execute(sprintf('mkdir -p %s', $dir));
@@ -136,7 +152,6 @@ class Build extends Command
         $this->env->execute('rm -rf generated/*');
 
         $this->env->log("Enabling all modules");
-        $this->env->execute("cd bin/; /usr/bin/php ./magento module:enable --all");
 
         if (!$this->getBuildOption(self::BUILD_OPT_SKIP_DI_COMPILATION)) {
             $this->env->log("Running DI compilation");
@@ -144,12 +159,6 @@ class Build extends Command
         } else {
             $this->env->log("Skip running DI compilation");
         }
-    }
-
-    private function importConfiguration()
-    {
-        $this->env->log('Import configuration');
-        $this->env->execute('cd bin/; /usr/bin/php ./magento app:config:import');
     }
 
     /**
@@ -176,6 +185,71 @@ class Build extends Command
         return isset($this->buildOptions[$key]) ? $this->buildOptions[$key] : false;
     }
 
+    public function deployStaticContent()
+    {
+        $configFile = Environment::MAGENTO_ROOT . 'app/etc/config.php';
+        if (file_exists($configFile)) {
+            $config = include $configFile;
+
+            $flattenedConfig = $this->flatten($config);
+            $websites = $this->filter($flattenedConfig, 'scopes/websites', false);
+            $stores = $this->filter($flattenedConfig, 'scopes/stores', false);
+
+            $locales = [];
+            $locales = array_merge($locales, $this->filter($flattenedConfig, 'general/locale/code'));
+            $locales = array_merge(
+                $locales,
+                $this->filter($flattenedConfig, 'admin_user/locale/code', false)
+            );
+            $locales[] = 'en_US';
+            $locales = array_unique($locales);
+
+            if (count($stores) === 0 && count($websites) === 0) {
+                $this->env->log("No stores/website/locales found in config.php");
+                return;
+            }
+
+            $SCDLocales = implode(' ', $locales);
+
+            $excludeThemesOptions = '';
+            if ($this->getBuildOption(self::BUILD_OPT_SCD_EXCLUDE_THEMES)) {
+                $themes = preg_split("/[,]+/", $this->getBuildOption(self::BUILD_OPT_SCD_EXCLUDE_THEMES));
+                if (count($themes) > 1) {
+                    $excludeThemesOptions = "--exclude-theme=" . implode(' --exclude-theme=', $themes);
+                } elseif (count($themes) === 1) {
+                    $excludeThemesOptions = "--exclude-theme=" . $themes[0];
+                }
+            }
+
+            $threads = $this->getBuildOption(self::BUILD_OPT_SCD_THREADS)
+                ? "{$this->getBuildOption(self::BUILD_OPT_SCD_THREADS)}"
+                : '0';
+
+            try {
+                $logMessage = $SCDLocales
+                    ? "Generating static content for locales: $SCDLocales"
+                    : "Generating static content.";
+                $logMessage .= $excludeThemesOptions ? "\nExcluding Themes: $excludeThemesOptions" : "";
+                $logMessage .= $threads ? "\nUsing $threads Threads" : "";
+
+                $this->env->log($logMessage);
+
+                $parallelCommands = "";
+                foreach ($locales as $locale) {
+                    // @codingStandardsIgnoreStart
+                    $parallelCommands .= "php ./bin/magento setup:static-content:deploy -f $excludeThemesOptions $locale {$this->verbosityLevel}" . '\n';
+                    // @codingStandardsIgnoreEnd
+                }
+                $this->env->execute("printf '$parallelCommands' | xargs -I CMD -P " . (int)$threads . " bash -c CMD");
+            } catch (\Exception $e) {
+                $this->env->log($e->getMessage());
+                exit(5);
+            }
+        } else {
+            $this->env->log("Skipping static content deploy");
+        }
+    }
+
     private function generateFreshStaticContent()
     {
         $excludeThemesOptions = $this->staticDeployExcludeThemes
@@ -188,5 +262,41 @@ class Build extends Command
         $this->env->execute(
             "/usr/bin/php ./bin/magento setup:static-content:deploy -f $jobsOption $excludeThemesOptions {$this->verbosityLevel}"
         );
+    }
+
+    private function composerDumpAutoload()
+    {
+        $this->env->execute('composer dump-autoload -o');
+    }
+
+    private function flatten($array, $prefix = '')
+    {
+        $result = [];
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $result = $result + $this->flatten($value, $prefix . $key . '/');
+            } else {
+                $result[$prefix . $key] = $value;
+            }
+        }
+        return $result;
+    }
+
+    private function filter($array, $pattern, $ending = true)
+    {
+        $filteredResult = [];
+        $length = strlen($pattern);
+        foreach ($array as $key => $value) {
+            if ($ending) {
+                if (substr($key, -$length) === $pattern) {
+                    $filteredResult[$key] = $value;
+                }
+            } else {
+                if (substr($key, 0, strlen($pattern)) === $pattern) {
+                    $filteredResult[$key] = $value;
+                }
+            }
+        }
+        return array_unique(array_values($filteredResult));
     }
 }
